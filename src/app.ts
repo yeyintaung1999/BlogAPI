c1086adcdfd625ddf993d9b8b3050e1a1a7030bd
Ye Yint Aung <yeyint_2@coremobile.co.jp>
2026-08-06 18:28:58 +0900

    changed origin cors

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,17 @@ import airoute from "./modules/ai/routes/ai.routes.js"
 import authroute from "./modules/auth/routes/auth.routes.js"
 import postroute from "./modules/post/routes/post.routes.js"
 import { errorMiddleware } from "./middlewares/error.middleware.js";
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://13.57.33.2",
+    credentials: true,
+  })
+);
 
 app.use(cookieParser())
 app.use(express.json());
